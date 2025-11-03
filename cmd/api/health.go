e0fc488a28863d94c9fe0f949ee5e138e9fae793
Anuj Gupta <anujgupta@mac.mynetworksettings.com>
2025-11-03 17:13:53 -0500

    learned about json in go

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -1,12 +1,17 @@
 package main
 
 import (
-	"log"
 	"net/http"
 )
 
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	if err := writeJSON(w, http.StatusOK, "ok"); err != nil {
-		log.Print(err.Error())
+	data := map[string]string{
+		"status":  "ok",
+		"env":     app.config.env,
+		"version": version,
+	}
+
+	if err := writeJSON(w, http.StatusOK, data); err != nil {
+		writeJSONError(w, http.StatusInternalServerError, "err.Error()")
 	}
 }
