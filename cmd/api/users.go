2c2b939df8266368d5293c5fb6c05a519a2f28b6
Anuj <apgupta3091@gmail.com>
2025-11-21 15:43:02 -0500

    added following

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	"strconv"
 
@@ -8,29 +9,97 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
+type userKey string
+
+var userCtx userKey = "user"
+
 func (app *application) getUserHandler(w http.ResponseWriter, r *http.Request) {
-	userID, err := strconv.ParseInt(chi.URLParam(r, "userID"), 10, 64)
-	if err != nil {
+	user := app.getUserFromCtx(r)
+
+	if err := app.jsonResponse(w, http.StatusOK, user); err != nil {
+		app.internalServerError(w, r, err)
+	}
+
+}
+
+type FollowUser struct {
+	UserID int64 `json:"user_id"`
+}
+
+func (app *application) followUserHandler(w http.ResponseWriter, r *http.Request) {
+	followerUser := app.getUserFromCtx(r)
+
+	var payload FollowUser
+	if err := readJSON(w, r, &payload); err != nil {
 		app.badRequestResponse(w, r, err)
 		return
 	}
+
 	ctx := r.Context()
 
-	user, err := app.store.Users.GetByID(ctx, userID)
-	if err != nil {
+	if err := app.store.Followers.Follow(ctx, followerUser.ID, payload.UserID); err != nil {
 		switch err {
-		case store.ErrNotFound:
-			app.badRequestResponse(w, r, err)
-			return
+		case store.ErrConflict:
+			app.conflictResponse(w, r, err)
 		default:
 			app.internalServerError(w, r, err)
-			return
 		}
+		return
 	}
 
-	if err := app.jsonResponse(w, http.StatusOK, user); err != nil {
+	if err := app.jsonResponse(w, http.StatusNoContent, nil); err != nil {
+		app.internalServerError(w, r, err)
+	}
+}
+
+func (app *application) unfollowUserHandler(w http.ResponseWriter, r *http.Request) {
+	unfollowedUser := app.getUserFromCtx(r)
+
+	var payload FollowUser
+	if err := readJSON(w, r, &payload); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	ctx := r.Context()
+
+	if err := app.store.Followers.Unfollow(ctx, unfollowedUser.ID, payload.UserID); err != nil {
 		app.internalServerError(w, r, err)
 		return
 	}
 
+	if err := app.jsonResponse(w, http.StatusNoContent, nil); err != nil {
+		app.internalServerError(w, r, err)
+	}
+}
+
+func (app *application) userContextMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		userID, err := strconv.ParseInt(chi.URLParam(r, "userID"), 10, 64)
+		if err != nil {
+			app.badRequestResponse(w, r, err)
+			return
+		}
+
+		ctx := r.Context()
+
+		user, err := app.store.Users.GetByID(ctx, userID)
+		if err != nil {
+			switch err {
+			case store.ErrNotFound:
+				app.notFoundResponse(w, r, err)
+			default:
+				app.internalServerError(w, r, err)
+			}
+			return
+		}
+
+		ctx = context.WithValue(ctx, userCtx, user)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func (app *application) getUserFromCtx(r *http.Request) *store.User {
+	user, _ := r.Context().Value(userCtx).(*store.User)
+	return user
 }
