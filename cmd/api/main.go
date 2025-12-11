5eafda410ff52526c1a37a2c0d388393a80987d7
Anuj <apgupta3091@gmail.com>
2025-12-10 21:53:51 -0500

    learning basic auth

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -40,6 +40,12 @@ func main() {
 			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
 			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
 		},
+		auth: authConfig{
+			basic: basicConfig{
+				user: env.GetString("AUTH_BASIC_USER", "admin"),
+				pass: env.GetString("AUTH_BASIC_PASS", "admin"),
+			},
+		},
 		env: env.GetString("ENV", "development"),
 		mail: mailConfig{
 			exp:       time.Hour * 24 * 3, // 3 days
